8e208f71c54df39dd5bbc5286b30bcae4b8a64b2
Trung luc <trungluc217@gmail.com>
2021-08-03 01:39:55 +0700

    Fix: roadmap

--- src/containers/Roadmap.tsx
+++ src/containers/Roadmap.tsx
@@ -1,49 +1,75 @@
-import React, { FC } from 'react'
-import { SectionTitle } from 'components/SectionTitle'
-import { TimelinePoint } from 'components/TimelinePoint'
+import React, { FC } from "react";
+import { SectionTitle } from "components/SectionTitle";
+import { TimelinePoint } from "components/TimelinePoint";
 
-import s from './Roadmap.module.css'
+import s from "./Roadmap.module.css";
 
-export const Roadmap:FC<{}> = () => {
+export const Roadmap: FC<{}> = () => {
   return (
-    <section id='roadmap' className={s.roadmap}>
+    <section id="roadmap" className={s.roadmap}>
       <SectionTitle>Roadmap</SectionTitle>
       <p className={s.description}>$AT Roadmap</p>
       <div className={s.timeline}>
-        <TimelinePoint 
-          date='Q2 2021'
-          content={[
-            'Farming & Staking Pools',
-            'Initial Token Release',
-          ]}
-        />
+        <div className={s.timeline__wrapper}>
+          <TimelinePoint
+            date="Q2 2021"
+            content={["Farming & Staking Pools", "Initial Token Release"]}
+          />
 
-         <TimelinePoint 
-          date='Q3 2021'
-          content={[
-            'Initial Launchpad Release (ALPHA)',
-            'Liquidity Aggregator Starting Development',
-          ]}
-        />
+          <TimelinePoint
+            date="Q3 2021"
+            content={[
+              "Initial Launchpad Release (ALPHA)",
+              "Liquidity Aggregator Starting Development",
+            ]}
+          />
 
-         <TimelinePoint 
-          date='Q4 2021'
-          content={[
-            'Launchpad Release Stable v1',
-            'Initial Liquidity Aggregator Release (ALPHA)',
-            'NFT Marketplace Starting Development',
-          ]}
-        />
+          <TimelinePoint
+            date="Q4 2021"
+            content={[
+              "Launchpad Release Stable v1",
+              "Initial Liquidity Aggregator Release (ALPHA)",
+              "NFT Marketplace Starting Development",
+            ]}
+          />
 
-         <TimelinePoint 
-          date='Q1 2022'
-          content={[
-            'Initial NFT Marketplace Release (ALPHA)',
-            'Liquidity Aggregator Release Stable v1',
-            'NFT Marketplace Release Stable V1',
-          ]}
-        />
+          <TimelinePoint
+            date="Q1 2022"
+            content={[
+              "Initial NFT Marketplace Release (ALPHA)",
+              "Liquidity Aggregator Release Stable v1",
+              "NFT Marketplace Release Stable V1",
+            ]}
+          />
+
+          <TimelinePoint
+            date="Q1 2022"
+            content={[
+              "Initial NFT Marketplace Release (ALPHA)",
+              "Liquidity Aggregator Release Stable v1",
+              "NFT Marketplace Release Stable V1",
+            ]}
+          />
+
+          <TimelinePoint
+            date="Q1 2022"
+            content={[
+              "Initial NFT Marketplace Release (ALPHA)",
+              "Liquidity Aggregator Release Stable v1",
+              "NFT Marketplace Release Stable V1",
+            ]}
+          />
+
+          <TimelinePoint
+            date="Q1 2022"
+            content={[
+              "Initial NFT Marketplace Release (ALPHA)",
+              "Liquidity Aggregator Release Stable v1",
+              "NFT Marketplace Release Stable V1",
+            ]}
+          />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
