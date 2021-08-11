7b8065bb59fc0af3bf4c5d3b7090975d3858548b
Trung Luc <trungluc217@gmail.com>
2021-08-11 13:35:45 +0700

    Fix: change layout ecosystem

--- src/containers/Ecosystem/index.tsx
+++ src/containers/Ecosystem/index.tsx
@@ -5,6 +5,7 @@ import launchpad from 'assets/img/launchpad.svg';
 import liquidity from 'assets/img/liquidity.svg';
 import farming from 'assets/img/farming.svg';
 import market from 'assets/img/market.svg';
+import wallet_screen from 'assets/img/wallet-screen.png';
 
 import './Ecosystem.css';
 /* import { Link } from 'react-router-dom'; */
@@ -16,22 +17,31 @@ export const Ecosystem = () => {
             <SectionHead frontText="Ecosystem" behindText="Ecosystem" />
             <p className="ecosystem__subtitle" >Part of the DooFinance Ecosystem</p>
             <div className="container ecosystem__grid" >
+                <img 
+                    className="grid-item ecosystem__screen"
+                    src={wallet_screen} 
+                    alt="wallet screen"
+                />
                 <GridItem
+                    className="ecosystem__launchpad"
                     icon={launchpad}
                     title="Lauchpad"
                     description="The IDO will give community members the chance to get in on the ground floor and claim a new token on the launchpad."
                 />
                 <GridItem
+                    className="ecosystem__liquidity"
                     icon={liquidity}
                     title="Liquidity Aggregator"
                     description="Traders use liquidity aggregators to find the best possible price across a variety of liquidity pools."
                 />
                 <GridItem
+                    className="ecosystem__farming"
                     icon={farming}
                     title="Farming/Staking"
                     description="With each IDO launched on our platform, a new farming pool will be deployed based on the launched token."
                 />
                 <GridItem
+                    className="ecosystem__market"
                     icon={market}
                     title="NFT Marketplace"
                     description="Users can easily create as many collectibles as they wish, sell them, or decide not to sell on this network."
@@ -48,17 +58,18 @@ export const Ecosystem = () => {
 /** Grid Item */
 
 interface GridItemProps{
+    className?:string;
     icon: any;
     title: string;
     description: string;
 }
 
 const GridItem:FC<GridItemProps> = ({
-    icon, title, description,
+    icon, title, description, className
 }) => {
     
     return (
-        <div className="grid-item" >
+        <div className={className ? `${className} grid-item` :"grid-item"} >
             <img className="grid-item__icon" src={icon} alt={title} />
             <div className="grid-item__content" >
                 <h3 className="grid-item__content__title" >{title}</h3>
